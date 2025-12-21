d4b066cd0d7152e2e8f0df542e9006c458d0f041
highghlow <highghlow@proton.me>
2025-12-21 21:56:44 +0300

    Added test suite

---
+++ b/tests/al-count-1.b
@@ -0,0 +1,21 @@
+[ This is a binary counter benchmark program, it runs   ]
+[ 671106031 instructions and is mostly unoptimisable.   ]
+[ As it runs it prints the letters of the alphabet with ]
+[ a delay that doubles between each letter.             ]
+[ Robert de Bath  2015, 2020                            ]
+
+>++++++++[>+++>>>++++++++<<<<-]>++<<
++[>                 // The loop flag
+    >>[>>]          // Find first zero
+    <[              // Check for the term flag
+        [->>+<<]    // if found move it up one
+        >>
+    >+.[->>+<<]<
+
+        -[<<]       // Decrement and finish on zero
+    ]>              // Back to the zero
+    +               // Make the zero a one
+    <<[-<<]         // Remove all the carried ones
+<]                  // and loop
+>+>>>>[[-]<<]<      // Clean up
++++++++++.[-]       // NL
--- bf/industrial-bf/test.sh
+++ bf/industrial-bf/test.sh
@@ -1,10 +1,74 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
-IFS=$'\n\t';
+IFS=$'\n\t'
 
-TEST_DIR="../../b/";
+TEST_DIR="../../b/tests"
 
-for i in $(find "$TEST_DIR" -type f); do
-        echo "$i"
-        ./ibf "$i" > /dev/null
+if [[ ! -z ${BF:-} ]]; then
+	echo -n
+elif which bf > /dev/null 2> /dev/null; then
+	BF=bf;
+elif which brainfuck > /dev/null 2> /dev/null; then
+	BF=brainfuck;
+fi
+
+if [[ ! -z ${CC:-} ]]; then
+	echo -n
+elif which cc > /dev/null 2> /dev/null; then
+	CC=cc;
+else
+	echo No cc found
+	echo Set \$CC to set it\'s path
+        exit 1
+fi
+
+scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+function finish {
+  rm -rf "$scratch"
+}
+trap finish EXIT
+
+if [[ -z ${1:-} ]]; then
+	pushd $TEST_DIR > /dev/null
+	tests=$(find -type f -name '*.b' -printf '%P\n')
+	popd > /dev/null
+else
+	tests=($1);
+fi
+
+for testname_r in ${tests[@]}; do
+	file=$TEST_DIR/$testname_r
+	testname=$(echo $testname_r | sed 's&/&_&g')
+	echo Running $testname...;
+
+	inp="< $testname.in"
+	in=$TEST_DIR/$testname_r.in
+	if [ ! -e $in ]; then
+		inp=""
+		in=/dev/null
+	fi
+
+	if [ -e $TEST_DIR/$testname_r.out ]; then
+		cp $TEST_DIR/$testname_r.out $scratch/$testname.ref.out
+	else
+                if [[ -z ${BF:-} ]]; then
+                        echo No reference interpreter found
+                        echo Set \$BF to set it\'s path
+                        exit 1
+                fi
+		echo '  >' ${BF} $testname $inp
+		${BF} $file > $scratch/$testname.ref.out < $in
+		if [[ -n ${GENERATE_OUTS:-} ]]; then
+			echo "  Warning: generating outfile"
+			cp $scratch/$testname.ref.out $TEST_DIR/$testname_r.out
+		fi
+	fi
+
+	echo '  >' ibf $testname $inp
+	./ibf $file > $scratch/$testname.got.out
+
+	diff $scratch/$testname.ref.out $scratch/$testname.got.out
 done
+
+echo All OK
