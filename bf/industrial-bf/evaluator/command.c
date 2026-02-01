ce43c851957aaa0c5bcc0811b7f2df6533b81df3
highghlow <highghlow@proton.me>
2026-02-01 22:12:24 +0300

    Inlined command.c

--- bf/industrial-bf/evaluator/command.c
+++ bf/industrial-bf/evaluator/command.c
@@ -1,23 +1,23 @@
-uint8_t CMD_cmd(uint8_t *inst) {
+static inline uint8_t CMD_cmd(uint8_t *inst) {
         return inst[0];
 }
 
-uint8_t CMD_simple_arg(uint8_t *inst) {
+static inline uint8_t CMD_simple_arg(uint8_t *inst) {
         return inst[1];
 }
 
-uint16_t CMD_rol_arg(uint8_t *inst) {
+static inline uint16_t CMD_rol_arg(uint8_t *inst) {
         return ((uint16_t)inst[1])+1;
 }
 
-int16_t CMD_copy_offset(uint8_t *inst) {
+static inline int16_t CMD_copy_offset(uint8_t *inst) {
         return ntohs(*(uint16_t*)&inst[1]);
 }
 
-int8_t CMD_copy_val(uint8_t *inst) {
+static inline int8_t CMD_copy_val(uint8_t *inst) {
         return inst[3];
 }
 
-uint64_t CMD_wide_arg(uint8_t *inst) {
+static inline uint64_t CMD_wide_arg(uint8_t *inst) {
         return ntohll((*(uint64_t*)&inst[0]))&0x00ffffffffffffff;
 }
