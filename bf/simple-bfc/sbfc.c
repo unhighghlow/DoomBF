65de42c3f2d994538783a2aee2e43d6356053070
highghlow <highghlow@proton.me>
2026-02-09 14:36:41 +0000

    Added sbfc

---
+++ bf/simple-bfc/sbfc.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void  usage(char*);
+FILE *safe_fopen(char*, char*);
+void  out_str(char*);
+
+FILE *in_fp;
+FILE *out_fp;
+
+#define o(s) out_str(s "\n"); break;
+
+int main(int argc, char *argv[]) {
+        char chr;
+
+        if (argc < 2 || argc > 3) usage(argv[0]);
+
+        in_fp = safe_fopen(argv[1], "rb");
+        out_fp = 0;
+        if (argc > 2) {
+                out_fp = safe_fopen(argv[2], "w");
+        }
+
+        out_str(
+                "#include <stdio.h>\n"
+                "int main() {\n"
+                "char a[0x10000];\n"
+                "char *p = a;\n"
+        );
+        while (fread(&chr, 1, 1, in_fp)) {
+                switch (chr) {
+                case '+': o("p[0]++;")
+                case '-': o("p[0]--;")
+                case '<': o("p--;")
+                case '>': o("p++;")
+                case '.': o("putchar(p[0]);")
+                case ',': o("getc(p);")
+                case '[': o("while(*p){")
+                case ']': o("}")
+                default: break;
+                }
+        }
+        out_str(
+                "}\n"
+        );
+
+        fclose(in_fp);
+        if (out_fp) fclose(out_fp);
+
+        return 0;
+}
+
+void out_str(char *s) {
+        if (!out_fp) {
+                printf("%s", s);
+                return;
+        }
+        fwrite(s, 1, strlen(s), out_fp);
+}
+
+FILE *safe_fopen(char *filename, char *mode) {
+        FILE *out;
+
+        out = fopen(filename, mode);
+        if (!out) {
+                perror("cannot open file");
+                exit(1);
+        }
+        return out;
+}
+
+void usage(char *exec) {
+        printf("usage: %s in.b [out.b]\n", exec);
+        exit(2);
+}
