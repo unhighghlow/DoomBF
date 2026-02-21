ad215967bf9407daae1b53e7becc9431684a143c
highghlow <highghlow@proton.me>
2026-02-21 17:03:11 +0300

    Fixed help and license

--- bf/simple-bfc/sbfc.c
+++ bf/simple-bfc/sbfc.c
@@ -1,3 +1,16 @@
+/* sbfc -- Convert brainfuck programs to C.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE. */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -71,6 +84,9 @@ FILE *safe_fopen(char *filename, char *mode) {
 }
 
 void usage(char *exec) {
-        printf("usage: %s in.b [out.b]\n", exec);
+        printf("Usage: %s PROGRAM [OUT]\n", exec);
+        printf("Convert brainfuck programs to C.\n");
+        printf("\n");
+        printf("With no OUT, or when OUT is -, write to standard output.\n");
         exit(2);
 }
