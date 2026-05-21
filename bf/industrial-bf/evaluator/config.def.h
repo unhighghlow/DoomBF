0e3ea8d5bdc7287383bfd9c575a2a8fcd8f56c6f
Ivan Sakaev <x-sakaev@yandex.ru>
2026-05-21 17:13:54 +0300

    Fix too big moving could cause bugs with infinite tape

--- bf/industrial-bf/evaluator/config.def.h
+++ bf/industrial-bf/evaluator/config.def.h
@@ -1,14 +1,13 @@
 #define PAGE_SIZE_POWER 24 /* 1. The page size is 2**PAGE_SIZE_POWER \
                               2. This value must be greater than of equal to 8 */
-#define CELL uint8_t
+#define CELL uint8_t  // Changing this value may cause bugs
 
 // #define DISABLE_ROLLING /* Disables common statement shorthands, such as ^ or 0 */
 
 // ROLLING_TYPE should be signed
 typedef int32_t ROLLING_TYPE;
-#define ROLLING_TYPE_MAX INT32_MAX
-#define ROLLING_TYPE_MIN INT32_MIN
-#define ROLLING_SIZE sizeof(ROLLING_TYPE)
+#define __ROLLING_TYPE_MAX INT32_MAX
+#define __ROLLING_TYPE_MIN INT32_MIN
 
 // #define DEBUGGER /* Enables the -d option
 //                     Disabled by default because it
@@ -18,3 +17,9 @@ typedef int32_t ROLLING_TYPE;
 // #define   DEBUGGER_TAPE_VIEW     8
 #define ASSERTS
 #define CELL_FORMAT_STRING "%2x"
+
+
+// Don't change:
+#define ROLLING_SIZE sizeof(ROLLING_TYPE)
+#define ROLLING_TYPE_MAX ((1 << PAGE_SIZE_POWER) < __ROLLING_TYPE_MAX ? (1 << PAGE_SIZE_POWER) : __ROLLING_TYPE_MAX)
+#define ROLLING_TYPE_MIN (-ROLLING_TYPE_MAX-1)
